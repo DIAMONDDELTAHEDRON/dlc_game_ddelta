fce5d0eb6b69e501a4360453aeb25974d845da51
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-09-07 12:44:45 -0400

    Proper event for the board pushblocks and board-related sfx.

--- mod.lua
+++ mod.lua
@@ -1,133 +1,50 @@
 function Mod:init()
-    --print("Loaded "..self.info.name.."!")
-
-Utils.hook(PushBlock, "init", function(orig, self, x, y, shape, properties, sprite, solved_sprite)
-    orig(self, x, y, shape, properties, sprite, solved_sprite)
-
-    self.throw_reticle = Sprite("world/events/sword/throw_reticle")
-    self.throw_reticle:setScale(2)
-    
-    local sprite_b = properties["sprite"] or sprite or "world/events/sword/pushableblock"
-    self.carry = Sprite(sprite_b)
-    self.carry.y = -5
-end)
-
-Utils.hook(PushBlock, "update", function(orig, self)
-    orig(self)
-    if self.state == "CARRIED" then
-
-        local p = Game.world.player
-        local pf = p.facing
-
-        local a, b = 0, 0
-        if pf == "up" then b = -82 end
-        if pf == "down" then b = 82 end
-        if pf == "left" then a = -82 end
-        if pf == "right" then a = 82 end
-
-        local c, r = Mod:getSquare(p.x + a, p.y + b)
-
-        self.throw_reticle.x = c * 32
-        self.throw_reticle.y = r * 32
-
-        if Input.pressed("confirm") then
-
-            p.actor.default = "walk"
-            p:resetSprite()
-
-            self.x, self.y = self.throw_reticle.x, self.throw_reticle.y
-            Game.world:removeChild(self.throw_reticle)
-            p:removeChild(self.carry)
-            self:playPushSound()
-            self.solid = true
-            self.visible = true
-            self.state = "IDLE"
-            p.carry = nil
-        end
-    end
-end)
-
-Utils.hook(PushBlock, "onInteract", function(orig, self, chara, facing)
-    if self.solid and chara.actor.id == "board_susie" then
-
-        self:playPushSound()
-        chara.actor.default = "walk_armsup"
-        chara:resetSprite()
-        chara.carry = true
-
-        chara:addChild(self.carry)
-
-        Game.world:addChild(self.throw_reticle)
-        self.throw_reticle.layer = chara.layer
-
-        self.solid = false
-        self.visible = false
-        self.state = "CARRIED"
-        return true
-    elseif chara.actor.id ~= "board_kris" then
-        return true
-    end
-    self:playPushSound()
-
-    if self.state ~= "IDLE" then return true end
-
-    if not self:checkCollision(facing) then
-        self:onPush(facing)
-    else
-        self:onPushFail(facing)
-    end
-
-    return true
-end)
-
-
-Utils.hook(World, "onKeyPressed", function(orig, self, key)
-    if Kristal.Config["debug"] and Input.ctrl() then
-        if key == "m" then
-            if self.music then
-                if self.music:isPlaying() then
-                    self.music:pause()
-                else
-                    self.music:resume()
+    Utils.hook(World, "onKeyPressed", function(orig, self, key)
+        if Kristal.Config["debug"] and Input.ctrl() then
+            if key == "m" then
+                if self.music then
+                    if self.music:isPlaying() then
+                        self.music:pause()
+                    else
+                        self.music:resume()
+                    end
                 end
             end
-        end
-        if key == "s" then
-            local save_pos = nil
-            if Input.shift() then
-                save_pos = {self.player.x, self.player.y}
+            if key == "s" then
+                local save_pos = nil
+                if Input.shift() then
+                    save_pos = {self.player.x, self.player.y}
+                end
+                if Game:isLight() or Game:getConfig("smallSaveMenu") then
+                    self:openMenu(SimpleSaveMenu(Game.save_id, save_pos))
+                else
+                    self:openMenu(SaveMenu(save_pos))
+                end
             end
-            if Game:isLight() or Game:getConfig("smallSaveMenu") then
-                self:openMenu(SimpleSaveMenu(Game.save_id, save_pos))
-            else
-                self:openMenu(SaveMenu(save_pos))
+            if key == "n" then
+                NOCLIP = not NOCLIP
             end
         end
-        if key == "n" then
-            NOCLIP = not NOCLIP
-        end
-    end
 
-    if Game.lock_movement then return end
+        if Game.lock_movement then return end
 
-    if self.state == "GAMEPLAY" then
-        if Input.isConfirm(key) and self.player and not self:hasCutscene() then
-            if self.player:interact() then
-                Input.clear("confirm")
+        if self.state == "GAMEPLAY" then
+            if Input.isConfirm(key) and self.player and not self:hasCutscene() then
+                if self.player:interact() then
+                    Input.clear("confirm")
+                end
+            elseif Input.isMenu(key) and not self:hasCutscene() then
+                self:openMenu(nil, WORLD_LAYERS["ui"] + 1)
+                if not self.in_game then
+                    Input.clear("menu")
+                end
             end
-        elseif Input.isMenu(key) and not self:hasCutscene() then
-            self:openMenu(nil, WORLD_LAYERS["ui"] + 1)
-            if not self.in_game then
-                Input.clear("menu")
+        elseif self.state == "MENU" then
+            if self.menu and self.menu.onKeyPressed then
+                self.menu:onKeyPressed(key)
             end
         end
-    elseif self.state == "MENU" then
-        if self.menu and self.menu.onKeyPressed then
-            self.menu:onKeyPressed(key)
-        end
-    end
-end)
-
+    end)
 end
 
 function Mod:getSquare(x, y)
---
+++ scripts/world/events/pushblock_board.lua
@@ -0,0 +1,263 @@
+--- A Pushable Block! Collision for Pushblocks can be created by adding a `blockcollision` layer to a map. \
+--- `PushBlockBoard` is an [`Event`](lua://Event.init) - naming an object `pushblock` on an `objects` layer in a map creates this object. \
+--- See this object's Fields for the configurable properties on this object.
+--- 
+---@class PushBlockBoard : Event
+---
+---@field default_sprite    string      *[Property `sprite`]* An optional custom sprite the block should use
+---@field solved_sprite     string      *[Property `solvedsprite`]* An optional custom solve sprite the block uses
+---
+---@field solid             boolean     
+---
+---@field push_dist         number      *[Property `pushdist`]* The number of pixels the block moves per push (Defaults to `40`, one tile)
+---@field push_timer        number      *[Property `pushtime`]* The time the block takes to complete a push, in seconds (Defaults to `0.2`)
+---
+---@field push_sound        string      *[Property `pushsound`]* The name of the sound file to play when the block is pushed (Defaults to `pushsound`)
+---@field collide_sound     string      *[Property `collidesound`]* The name of the sound file to play when the block cannot be pushed (Defaults to `collidesound`)
+---@field lift_sound        string      *[Property `liftsound`]* The name of the sound file to play when the block is lifted up by Susie (Defaults to `liftsound`)
+---
+---@field press_buttons     boolean     *[Property `pressbuttons`]* Unused (Defaults to `true`)
+---
+---@field lock_in_place     boolean     *[Property `lock`]* Whether the block gets locked in place when in a solved state (Defaults to `false`)
+---
+---@field input_lock        boolean     *[Property `inputlock`]* Whether the player's input's are locked while the block is being pushed
+---
+---@field start_x           number      Initial position of the block
+---@field start_y           number      Initial position of the block
+---
+---@field state             string      The current state of the Pushblock - value can be IDLE, PUSH, RESET, or LIFT
+---
+---@field solved            boolean     Whether the pushblock is in a solved state
+---
+---@overload fun(...) : PushBlockBoard
+local PushBlockBoard, super = Class(Event)
+
+function PushBlockBoard:init(x, y, shape, properties, sprite, solved_sprite)
+    super.init(self, x, y, shape)
+
+    properties = properties or {}
+
+    self.default_sprite = properties["sprite"] or sprite or "world/events/sword/pushableblock"
+    self.solved_sprite = properties["solvedsprite"] or properties["sprite"] or solved_sprite or sprite or "world/events/sword/pushableblock"
+
+    self:setSprite(self.default_sprite)
+    self.solid = true
+
+    -- Options
+    self.push_dist = properties["pushdist"] or 32
+    self.push_time = properties["pushtime"] or 0.2
+
+    self.push_sound = properties["pushsound"] or "wing"
+    self.collision_sound = properties["collisionsound"] or "impact"
+    self.lift_sound = properties["liftsound"] or "board/lift"
+
+    self.press_buttons = properties["pressbuttons"] ~= false
+
+    self.lock_in_place = properties["lock"] or false
+    self.input_lock = properties["inputlock"]
+
+    -- State variables
+    self.start_x = self.x
+    self.start_y = self.y
+
+    -- IDLE, PUSH, RESET, LIFT
+    self.state = "IDLE"
+
+    self.solved = false
+	
+    self.throw_reticle = Sprite("world/events/sword/throw_reticle")
+    self.throw_reticle:setScale(2)
+    
+    local sprite_b = properties["sprite"] or sprite or "world/events/sword/pushableblock"
+    self.carry = Sprite(sprite_b)
+    self.carry.y = -5
+end
+
+function PushBlockBoard:update()
+    super.update(self)
+
+    if self.state == "LIFT" then
+        local p = Game.world.player
+        local pf = p.facing
+
+        local a, b = 0, 0
+        if pf == "up" then b = -82 end
+        if pf == "down" then b = 82 end
+        if pf == "left" then a = -82 end
+        if pf == "right" then a = 82 end
+
+        local c, r = Mod:getSquare(p.x + a, p.y + b)
+
+        self.throw_reticle.x = c * 32
+        self.throw_reticle.y = r * 32
+
+        if Input.pressed("confirm") then
+            p.actor.default = "walk"
+            p:resetSprite()
+
+            self.x, self.y = self.throw_reticle.x, self.throw_reticle.y
+            Game.world:removeChild(self.throw_reticle)
+            p:removeChild(self.carry)
+            self:playPushSound()
+            self.solid = true
+            self.visible = true
+            self.state = "IDLE"
+            p.carry = nil
+        end
+    end
+end
+
+function PushBlockBoard:onInteract(chara, facing)
+    if self.solid and chara.actor.id == "board_susie" then
+
+        self:playLiftSound()
+        chara.actor.default = "walk_armsup"
+        chara:resetSprite()
+        chara.carry = true
+
+        chara:addChild(self.carry)
+
+        Game.world:addChild(self.throw_reticle)
+        self.throw_reticle.layer = chara.layer
+
+        self.solid = false
+        self.visible = false
+        self.state = "LIFT"
+        return true
+    elseif chara.actor.id ~= "board_kris" then
+        return true
+    end
+
+    if self.state ~= "IDLE" then return true end
+
+    if not self:checkCollision(facing) then
+        self:onPush(facing)
+        self:playPushSound()
+    else
+        self:onPushFail(facing)
+        self:playCollisionSound()
+    end
+
+    return true
+end
+
+function PushBlockBoard:playPushSound()
+    if self.push_sound and self.push_sound ~= "" then
+        for i = 0, 3-1 do
+            Game.world.timer:after((1 + (2 * i))/30, function()
+                Assets.playSound(self.push_sound, 1, 1.5 + (i / 20))
+            end)
+        end
+    end
+end
+
+function PushBlockBoard:playCollisionSound()
+    if self.collision_sound and self.collision_sound ~= "" then
+        Assets.playSound(self.collision_sound, nil, 1.2)
+    end
+end
+
+function PushBlockBoard:playLiftSound()
+    if self.lift_sound and self.lift_sound ~= "" then
+        Assets.stopAndPlaySound(self.lift_sound)
+    end
+end
+
+function PushBlockBoard:checkCollision(facing)
+    local collided = false
+
+    local dx, dy = Utils.getFacingVector(facing)
+    local target_x, target_y = self.x + dx * self.push_dist, self.y + dy * self.push_dist
+
+    local x1, y1 = math.min(self.x, target_x), math.min(self.y, target_y)
+    local x2, y2 = math.max(self.x + self.width, target_x + self.width), math.max(self.y + self.height, target_y + self.height)
+
+    local bound_check = Hitbox(self.world, x1 + 1, y1 + 1, x2 - x1 - 2, y2 - y1 - 2)
+
+    Object.startCache()
+    for _,collider in ipairs(Game.world.map.block_collision) do
+        if collider:collidesWith(bound_check) then
+            collided = true
+            break
+        end
+    end
+    if not collided then
+        self.collidable = false
+        collided = self.world:checkCollision(bound_check)
+        self.collidable = true
+    end
+    Object.endCache()
+
+    return collided
+end
+
+function PushBlockBoard:onPush(facing)
+    if self.solved then
+        if self.lock_in_place then
+            return
+        end
+
+        self.solved = false
+        self:onUnsolved()
+    end
+
+    local input_lock = Game:getConfig("pushBlockInputLock")
+    if self.input_lock ~= nil then
+        input_lock = self.input_lock
+    end
+
+    if input_lock then
+        Game.lock_movement = true
+    end
+
+    self.state = "PUSH"
+    local dx, dy = Utils.getFacingVector(facing)
+    self:slideTo(self.x + dx * self.push_dist, self.y + dy * self.push_dist, self.push_time, "linear", function()
+        self.state = "IDLE"
+        self:onPushEnd(facing)
+
+        if input_lock and not self.world.cutscene then
+            Game.lock_movement = false
+        end
+    end)
+end
+
+--- *(Override)* Called when the block enters a solved state
+function PushBlockBoard:onSolved()
+    self:setSprite(self.solved_sprite)
+end
+
+--- *(Override)* Called when the block stops being in a solved state
+function PushBlockBoard:onUnsolved()
+    self:setSprite(self.default_sprite)
+end
+
+--- *(Override)* Called when a block finishes being pushed
+function PushBlockBoard:onPushEnd(facing) end
+--- *(Override)* Called when a block cannot be pushed because of collision
+function PushBlockBoard:onPushFail(facing) end
+
+--- Fades the block out and returns it to its original position
+function PushBlockBoard:reset()
+    if self.solved then
+        self.solved = false
+        self:onUnsolved()
+    end
+
+    self.state = "RESET"
+    self.collidable = false
+    self.sprite:fadeToSpeed(0, 0.2, function()
+        self.x = self.start_x
+        self.y = self.start_y
+        self:onReset()
+        self.sprite:fadeToSpeed(1, 0.2, function()
+            self.collidable = true
+            self.state = "IDLE"
+        end)
+    end)
+end
+
+--- *(Override)* Called when the block is reset
+function PushBlockBoard:onReset() end
+
+return PushBlockBoard
